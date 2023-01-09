16b493bcf57f3e9fb5940894f0a84e50499e17c0
Milin Jadhav <awsmilin@gmail.com>
2023-01-09 18:33:53 +0530

    commit #2

--- script.js
+++ script.js
@@ -5,15 +5,23 @@
 let randomNum = Math.floor(Math.random() * 100 + 1);
 let tries = 1;
 
-//console.log(randomNum);
+console.log(randomNum);
 
 
 function guessNumber() {
 
     let a = document.getElementById("txtNum").value;
-    //    console.log(a);
+    //   console.log(a);
 
-    if (tries >= 5) {
+    if (a == randomNum) {
+        alert("Congratulations!!!! You have Guessed Correct Number");
+        //document.getElementById("txtNum").value="";
+        //document.getElementById("txtNum").disabled = true;
+        window.location.reload();
+
+    }
+
+    else if (tries >= 5) {
 
         alert(" Game Over..!!! Sorry you have exhausted all guesses better luck next time, Correct Number Was " + randomNum);
         //document.getElementById("txtNum").value="";
@@ -25,29 +33,15 @@ function guessNumber() {
         alert("Please enter a valid number between 1 to 100");
         tries++
         tries = tries
-        //          console.log("try no " + tries);
     }
     else if (a < randomNum) {
         alert("Guessed Number is lower...!!");
-        //     console.log("Guessed Number is lower." + a);
         tries++
         tries = tries
-        //        console.log("try no " + tries);
     }
     else if (a > randomNum) {
         alert("Guessed Number is Higher...!!!");
-        //       console.log("Guessed Number is Higher." + a);
         tries++
         tries = tries
-        //      console.log("try no " + tries);
     }
-    else if (a == randomNum) {
-        alert("Congratulations!!!! You have Guessed Correct Number");
-        console.log("You have Guessed Correct Number" + a);
-        //document.getElementById("txtNum").value="";
-        //document.getElementById("txtNum").disabled = true;
-        window.location.reload();
-
-    }
-
 }
